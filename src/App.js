445f05a2f784ff1f5f4cc36749b8d2f5f20f9161
Surendar Jaisankar <surendarjaisankar@gmail.com>
2024-09-26 10:57:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="App-logo" alt="logo">React</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
